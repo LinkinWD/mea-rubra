8e81a6f68ee1ad42bc51c8a6887c4fd84431fc78
Jarmo Lindström <yaramodesu@gmail.com>
2022-10-12 10:43:47 +0300

    background ok in header

--- src/components/Header.styles.ts
+++ src/components/Header.styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
 
+export const Section = styled.section`
+background: rgb(255,255,255) !important;
+background: radial-gradient(circle, rgba(255,255,255,1) 8%, rgba(255,182,182,1) 76%) !important;
+height: 80vh;
+`
+
 export const ImageDiv = styled.div`
-    background: grey;
+position:relative;
+
+border-radius: 50%;
+
+overflow:hidden;
+height:400px;
+
+
+img {
+
+    position: absolute;
+    top:40%;
+    left:40%;
+}
 `
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import beagle from '../assets/beagle.png'
-import { ImageDiv } from './Header.styles';
+import { ImageDiv, Section } from './Header.styles';
 
 
 export interface IHeaderProps {
@@ -15,23 +15,23 @@ export function Header (props: IHeaderProps) {
 
   return (
     
-    <section className='container text-center'>
+    <Section className='container text-center'>
         <div className="header">
         <h1 className='display-2'>Kennel</h1>
         </div>
          
-        <div className="row d-flex flex-row justify-content-center align-items-center" style={{height: '60vh'}}>
+        <div className="row d-flex flex-row justify-content-center align-items-center">
             <div className="text_area col-md-6 d-flex flex-column justify-content-center align-items-center p-5">
                 <p>Kennelin mea esittää</p>
                 <a href="#" className='btn btn-primary'>Tutustu koulutuksiin</a>
                 
             </div>
-            <ImageDiv className="image_area col-md-6 p-5">
+            <ImageDiv className="image_area col-md-6 p-5 ">
                 <img src={beagle} className='fluid-image col-10'/>
             </ImageDiv>
         </div>
      
-    </section>
+    </Section>
   );
   
 }
